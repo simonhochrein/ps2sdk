6f925d3c841a5435af48aebe5a9e12ebd16fbc70
Simon Hochrein <10032418+simonhochrein@users.noreply.github.com>
2025-05-01 08:21:19 -0500

    Add missing specifier

--- tools/srxfixup/src/readconf.c
+++ tools/srxfixup/src/readconf.c
@@ -345,7 +345,7 @@ static TokenTree *make_conf_vector(LowToken **lowtokens)
 		{
 			if (
 				*ltp->str != '@' && *ltp->str != '.' && *ltp->str != '_' && *ltp->str != '*'
-				&& isalnum(*((unsigned *)ltp->str)) == 0 )
+				&& isalnum(*((unsigned char *)ltp->str)) == 0 )
 			{
 				fprintf(stderr, "make_conf_vector(): unexcepted data '%s' line:%d col=%d\n", ltp->str, ltp->line, ltp->col);
 				exit(1);
